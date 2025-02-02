d6ee5d613bac9bfa377e831ca174f67c6cc5dcc9
benccooper <81181932+benccooper@users.noreply.github.com>
2025-02-02 01:25:31 -0800

    Update shared_scaffold.dart - allow actions on separate pages

--- lib/shared_scaffold.dart
+++ lib/shared_scaffold.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:travellista/nav_bar.dart';
 
-/// Scaffold wrapper that includes the navbar
+// Scaffold wrapper that includes the navbar
 class SharedScaffold extends StatelessWidget {
   final Widget body;
   final String title;
+  final List<Widget>? actions;
 
   const SharedScaffold({
     super.key,
     required this.body,
     required this.title,
+    this.actions,
   });
 
   @override
@@ -17,6 +19,7 @@ class SharedScaffold extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: actions, // <-- pass the list of actions to the AppBar
       ),
       bottomNavigationBar: NavBar(),
       body: body,
